ff9855caac3a3dc987895a3e2e3526136daa9951
Vlad Melisa-Andra <melisa.vlad@s.unibuc.ro>
2021-10-17 18:44:49 +0300

    Update 2

--- cautare.js
+++ cautare.js
@@ -9,12 +9,17 @@ app.use('/post', express.urlencoded({extended:false}));
 app.post("/post", function(req,res){
 	fs.readFile("retete.json", function(err, d) {
 		let p = JSON.parse(d);
+		let count=0;
 		res.write('<body style="margin:0px;">');
 		res.write('<div style="text-align:center;background:#333;min-height:200px;">');
 		for(let i = 0; i < p.length; i++) {
 			if(p[i]['titlu'].includes(req.body.nume))
+			{	count=1;
 				res.write('<a href="http://127.0.0.1:8080/reteta.html/?id='+p[i]['id']+'" style="color:#fff;" target="_blank">' + p[i].titlu +'</a><br>');
+			}
 		}
+		if(count==0)
+			res.write("Nu exista retete ce contin " + req.body.nume);
 		res.write('</div>');
 		res.write('</body>');
 
